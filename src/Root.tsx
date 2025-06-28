e16a49bfcb4f06f6d16b70ba79175309eb47c065
RTSaitama <rtsaitama@outlook.com>
2025-06-28 13:10:44 +0300

    pages folder

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,9 +5,9 @@ import {
   Navigate,
 } from 'react-router-dom';
 import { App } from './App';
-import { PeoplePage } from './components/PeoplePage';
-import { HomePage } from './components/HomePage';
-import { NotFoundPage } from './components/NotFoundPage';
+import { PeoplePage } from './pages/PeoplePage';
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 export const Root = () => (
   <Router>
     <Routes>
